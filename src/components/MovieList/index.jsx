e5022e45f61ea5ab8ef6d20162642da38e46e094
Jairo da Silvaclear <silva_jairo2009@yahoo.com.br>
2025-03-01 03:21:33 -0300

    continua...

--- src/components/MovieList/index.jsx
+++ src/components/MovieList/index.jsx
@@ -2,48 +2,60 @@ import React, { useState, useEffect } from 'react';
 import './index.scss';
 import axios from 'axios';
 
-const API_KEY = '2b22d3204682430cc136cbf2d8a4ce84';
-const API_URL = 'https://api.themoviedb.org/3/trending/movie/day';
-const IMAGE_BASE_URL = 'https://image.tmdb.org/t/p/w500';
-const options = {
-  method: 'GET',
-  headers: {
-    accept: 'application/json',
-    Authorization: 'eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIyYjIyZDMyMDQ2ODI0MzBjYzEzNmNiZjJkOGE0Y2U4NCIsIm5iZiI6MTcwMjY4ODg1MC44NSwic3ViIjoiNjU3Y2Y4NTI2YjVmYzIwNzBiYTdjZWRlIiwic2NvcGVzIjpbImFwaV9yZWFkIl0sInZlcnNpb24iOjF9.da4WbAvndleh2PkvmHmRkKJoc9WOHUDiali3C5GSI70'
-  }
-}
 
+const API_KEY = '2b22d3204682430cc136cbf2d8a4ce84';
+const BASE_URL = 'https://api.themoviedb.org/3';
+const IMAGE_BASE_URL = 'https://image.tmdb.org/t/p/w500';
+
+const categories = {
+  novosVideos: '/movie/now_playing',
+  topVideos: '/movie/top_rated',
+  destaques: '/movie/popular',
+  maisAcessados: '/trending/movie/week',
+  series: '/tv/popular'
+};
 
 const MovieList = () => {
-  const [movies, setMovies] = useState([]);
+  const [movies, setMovies] = useState({});
 
   const getMovies = async () => {
     try {
-      const response = await axios.get(API_URL, {
-        params: {
-          api_key: API_KEY,
-          language: 'pt-BR'
-        }
+      const requests = Object.entries(categories).map(async ([key, endpoint]) => {
+        const response = await axios.get(`${BASE_URL}${endpoint}`, {
+          params: { api_key: API_KEY, language: 'pt-BR' }
+        });
+        return { [key]: response.data.results };
       });
-      setMovies(response.data.results);
+
+      const results = await Promise.all(requests);
+      const moviesData = results.reduce((acc, data) => ({ ...acc, ...data }), {});
+      setMovies(moviesData);
+
     } catch (error) {
       console.error('Erro ao buscar filmes:', error);
     }
-  };
+  }
 
   useEffect(() => {
     getMovies();
   }, []);
 
   return (
-    <ul className='movie-list'>
-      {movies.map(movie => (
-        <li key={movie.id}>
-          <img src={`${IMAGE_BASE_URL}${movie.poster_path}`} alt={movie.title} />
-          <h3>{movie.title}</h3>
-        </li>
+    <div className="movie-container">
+      {Object.entries(movies).map(([category, movieList]) => (
+        <div key={category} className="movie-category">
+          <h2>{category.replace(/([A-Z])/g, ' $1').toUpperCase()}</h2>
+          <ul className='movie-list'>
+            {movieList?.map(movie => (
+              <li key={movie.id}>
+                <img src={`${IMAGE_BASE_URL}${movie.poster_path}`} alt={movie.title} />
+                <h3>{movie.title}</h3>
+              </li>
+            ))}
+          </ul>
+        </div>
       ))}
-    </ul>
+    </div>
   );
 };
 
